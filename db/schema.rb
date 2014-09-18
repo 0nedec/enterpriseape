275310fac347657423b2ee01149424460516d56f
Carlos Cedeno <Carlosantoniocedeno@gmail.com>
2014-09-18 19:57:27 +0000

    Added status functionality to invoice

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917204852) do
+ActiveRecord::Schema.define(version: 20140918183744) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140917204852) do
     t.string   "salesperson"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status_type"
   end
 
   create_table "users", force: true do |t|
